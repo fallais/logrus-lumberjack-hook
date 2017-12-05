eab085e57da278d8e56588df42707813c8f92d65
Francois ALLAIS <francois.allais@hotmail.com>
2017-12-05 17:31:18 +0100

    initial commit

---
+++ logrus_lumberjack.go
@@ -0,0 +1,53 @@
+package logrus_lumberjack
+
+import (
+  "fmt"
+
+  "gopkg.in/natefinch/lumberjack.v2"
+  "github.com/sirupsen/logrus"
+)
+
+// LumberjackHook stores the configuration of the hook
+type LumberjackHook struct {
+  logger *lumberjack.Logger
+}
+
+// NewHook creates a new LumberjackHook
+func NewHook(logger *lumberjack.Logger) (*LumberjackHook, error) {
+  if logger == nil {
+    return nil, fmt.Errorf("Logger cannot be nil")
+  }
+
+  return &LumberjackHook{
+    logger: logger,
+  }, nil
+}
+
+// Fire is called when a log event is fired.
+func (hook *LumberjackHook) Fire(entry *logrus.Entry) error {
+  // Convert the line to string
+  line, err := entry.String()
+  if err != nil {
+    return err
+  }
+
+  // Write the the logger
+  _, err = hook.logger.Write([]byte(line))
+  if err != nil {
+    return err
+  }
+
+  return nil
+}
+
+// Levels returns the available logging levels
+func (hook *LumberjackHook) Levels() []logrus.Level {
+  return []logrus.Level{
+    logrus.DebugLevel,
+    logrus.InfoLevel,
+    logrus.WarnLevel,
+    logrus.ErrorLevel,
+    logrus.FatalLevel,
+    logrus.PanicLevel,
+  }
+}
